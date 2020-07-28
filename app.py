37e841cbef825f849eca57f0487e41606c3afd6a
sushmanthnatha <42044878+sushmanthnatha@users.noreply.github.com>
2020-07-28 19:07:58 +0530

    added more features to output message

--- app.py
+++ app.py
@@ -27,7 +27,38 @@ def advisoryinfo():
 		df=pd.read_csv('../Travel_Risk_Analysis_Internship/country_isocode.csv')
 		dd = df[ df['Code']==results[0]['cc']]['Name'].values[0] 
 		ans+="\n "+dd
-		
+
+		if dd =='India':
+			l1 = results[0]['name']
+			l2 = results[0]['admin1']
+			l3 = df[ df['Code']==results[0]['cc']]['Name'].values[0]
+			ans+='\nCity - '+l1+'\nState/Neighbourhood - '+l2+'\nCountry - '+l3
+		else:
+			l1='None'
+			l2='None'
+			l3= dd
+			ans+='\n'+l3
+	
+		df1=pd.read_csv('../Travel_Risk_Analysis_Internship/data/district_info.csv')
+		df1.set_index('District',inplace=True)
+
+		df2=pd.read_csv('../Travel_Risk_Analysis_Internship/data/state_info.csv')
+		df2.set_index('State',inplace=True)
+
+		df3=pd.read_csv('../Travel_Risk_Analysis_Internship/data/country_info.csv')
+		df3.set_index('country',inplace=True)
+	
+	
+	
+		try:
+			dfff = pd.read_csv('../Travel_Risk_Analysis_Internship/advisory_info_from_api.csv')
+			dfff.set_index('Code',inplace=True)
+			dfff.head()
+
+			ans+="\n Risk Rating  : "+dfff.loc[ results[0]['cc'] , 'Risk_Rating']
+			ans+="\n Advice  : "+dfff.loc[ results[0]['cc'] , 'Advice']
+		except:
+			ans+="No advisory risk score and short advice found"
 
 		ans=ans.replace('\n','<br>')
 
